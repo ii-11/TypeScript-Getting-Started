4706bb16780abf31ff8508f3dda48c711e38f926
ii-11 <ivanovski@vivaldi.net>
2020-05-16 01:36:53 +0200

    Code examples with built-in types and function types

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,45 @@
 function startGame() {
     // starting a new game
-    var messageElement = document.getElementById('messages');
-    messageElement!.innerText = 'Welcome to MultiMath! Starting a new game ...';
+    let playerName: string | undefined = getInputValue('playername');
+    logPlayer(playerName);
+
+    postScore(100, playerName);
+    postScore(-5, playerName);
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if(inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    // function type/signature
+    let logger: (value: string) => void;
+
+    if(score < 0) {
+        logger = logError;
+    }
+    else {
+        logger = logMessage;
+    }
+
+    const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (msg: string) => console.log(msg);
+const logError = (err: string) => console.error(err);
